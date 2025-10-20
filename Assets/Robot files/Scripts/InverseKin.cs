ce47c722765683dbb1998e445d51b2280d4a7a83
GOODWOOLF <electromolot@mail.ru>
2025-10-20 23:24:27 +0500

    Init

---
+++ Assets/Robot files/Scripts/InverseKin.cs
@@ -0,0 +1,515 @@
+﻿using UnityEngine;
+using static UnityEngine.GraphicsBuffer;
+using UnityEngine.UI;
+using UnityEditor;
+using UnityEngine.UIElements;
+using UnityEditorInternal;
+using System.Globalization;
+using System.Threading;
+using System;
+using NUnit.Framework.Constraints;
+using System.Diagnostics;
+using System.Collections.Generic;
+using System.Collections;
+using UnityEditor.VersionControl;
+using UnityEditor.PackageManager;
+
+public class InverseKin : MonoBehaviour
+{
+    public bool realtime = false;
+    private List<float?[]> points;
+    private bool move = false;
+    //private bool flagwork = false;
+   // private float[] q = new float[6]; // start joints orientation
+
+   // private float[] qm = new float[6]; // trajectory
+
+    //public float q01, q02, q03, q04, q05, q06;
+
+    private float thetha1, thetha2, thetha3, thetha4, thetha5, thetha6;
+
+    // point to calculate ik
+    public float ox, oy , oz = 0;  
+    private float old_ox, old_oy, old_oz;  
+
+    public bool RobotOperate;
+
+    //private float Tm= 40; // trajectory steps
+
+    // links length
+    private float L1 = 177.19f*2.54f;
+    private float L2 = 175.98f*2.54f;
+    private float L3 = 448.51f*2.54f;
+    private float L4 = 142.04f*2.54f;
+    private float L5 = 534.97f*2.54f;
+    private float L6 = 277.59f*2.54f;
+
+    // to calculate inverse kin manually
+    public GameObject POINT;
+     private Vector3 point;
+    public GameObject RobotBase;
+    public GameObject configRobot;
+
+    void Start()
+    {
+        point = RobotBase.transform.InverseTransformPoint(POINT.transform.position);
+        oy = point.x * 1000;
+        oz = (point.y - 0.4f) * 1000;
+        ox = point.z * 1000;
+        old_ox = ox;
+        old_oy = oy;
+        old_oz = oz;
+        
+
+}
+    private void FixedUpdate()
+    {
+        point = RobotBase.transform.InverseTransformPoint(POINT.transform.position);
+        oy = point.x * 1000;
+        oz = (point.y - 0.4f) * 1000;
+        ox = point.z * 1000;
+        /* ox = float.Parse(NewX.text, CultureInfo.InvariantCulture);
+         oy = float.Parse(NewY.text, CultureInfo.InvariantCulture);
+         oz = float.Parse(NewZ.text, CultureInfo.InvariantCulture);*/
+        if (!move && checkNewXYZ())
+        {
+            CalculateIK();
+            old_ox = ox;
+            old_oy = oy;
+            old_oz = oz;
+        }
+
+    }
+    private bool checkNewXYZ()
+    {
+        if (realtime)//
+        {
+            return true;
+        }
+        else if ((ox == old_ox) && (oy == old_oy) && (oz == old_oz))
+        {
+            return false;
+        }
+        else
+        {
+            return true;
+        }
+    }
+    public void ChangeStateRobotOperate(bool state)
+    {
+        if (!state)
+        {
+            RobotOperate = true;
+        }
+        else
+        {
+            RobotOperate = false;
+        }
+    }
+    public void CalculateIK()
+    {
+        points = new();
+        Vector3 start = new Vector3(old_ox, old_oy, old_oz);
+        Vector3 current = start;
+        Vector3 end = new Vector3(ox, oy, oz);
+        Vector3 direction = (end - start).normalized;
+        float distance = Vector3.Distance(start, end);
+        print("Дистанция:" + distance);
+        float traveled = 0f;
+        float step = 100f;
+        if (!realtime)
+        {
+            while (traveled < distance)
+            {
+                float sizeVector = Mathf.Min(step, distance - traveled);// раст между кажд точкой
+                current += direction * sizeVector;
+                ox = current.x;
+                oy = current.y;
+                oz = current.z;
+                CalculateInverseKinematics();
+                traveled += sizeVector;
+            }
+            StartCoroutine(ModifyRobot(points));
+        }
+        else
+        {
+            CalculateInverseKinematics();
+            ModifyRobotRealtime();
+        }
+        
+    }
+    public void CalculateInverseKinematics()
+    {
+        try
+        {
+            print("start");
+            Stopwatch time = new Stopwatch();
+            time.Start();
+            // Transformation matrix of the ik point
+            /*
+            Matrix4x4 T = new Matrix4x4(new Vector4(1, 0, 0, ox),
+                                        new Vector4(0, 0, 1, oy),
+                                        new Vector4(0, -1, 0, oz),
+                                        new Vector4(0, 0, 0, 1));
+            */
+
+            Matrix4x4 T = new Matrix4x4(new Vector4(-1, 0, 0, ox),
+                                        new Vector4(0, 1, 0, oy),
+                                        new Vector4(0, 0, -1, oz),
+                                        new Vector4(0, 0, 0, 1));
+
+            // Rotation matrix of the ik point
+            Matrix4x4 R = new Matrix4x4(new Vector4(T[0, 0], T[1, 0], T[2, 0], 0),
+                                        new Vector4(T[0, 1], T[1, 1], T[2, 1], 0),
+                                        new Vector4(T[0, 2], T[1, 2], T[2, 2], 0),
+                                        new Vector4(0, 0, 0, 1));
+
+            Vector3 o = new Vector3(ox, oy, oz); // center point calculation
+            float xc = o.x - L6 * R[2, 0];
+            float yc = o.y - L6 * R[2, 1];
+            float zc = o.z - L6 * R[2, 2];
+            // calculate thetha1
+            thetha1 = Mathf.Atan2(yc, xc) * Mathf.Rad2Deg;
+
+            // calculate thetha3
+            float a = Mathf.Sqrt(L4 * L4 + L5 * L5);
+            float r = Mathf.Sqrt(xc * xc + yc * yc) - L1;
+            float s = zc - L2;
+            float b = Mathf.Sqrt(r * r + s * s);
+            float D = (L3 * L3 + a * a - b * b) / (2 * L3 * a);
+            float fi = Mathf.Acos(D) * Mathf.Rad2Deg;
+            float beta = 180 - fi;
+            float alpha = Mathf.Atan2(L4, L5) * Mathf.Rad2Deg;
+            // thetha3 = beta - alpha + 90; // Config I
+            thetha3 = -(beta + alpha) + 90; // Config II
+
+            // calculate thetha2
+            float fi2 = Mathf.Atan2(s, r) * Mathf.Rad2Deg;
+            float D1 = (L3 * L3 + b * b - a * a) / (2 * L3 * b);
+            float fi1 = Mathf.Acos(D1) * Mathf.Rad2Deg;
+            // thetha2 = fi2 - fi1; // Config I
+            thetha2 = fi2 + fi1; // Config II
+
+            // calculate thetha4, thetha5, thetha6
+
+            float alpha1 = 90, alpha2 = 0, alpha3 = 90, r1 = L1, r2 = L3, r3 = L4, d1 = L2, d2 = 0, d3 = 0;
+            // matrix T1
+            Matrix4x4 T1 = new Matrix4x4(new Vector4(Mathf.Cos(thetha1 * Mathf.Deg2Rad), -Mathf.Sin(thetha1 * Mathf.Deg2Rad) * Mathf.Cos(alpha1 * Mathf.Deg2Rad), Mathf.Sin(thetha1 * Mathf.Deg2Rad) * Mathf.Sin(alpha1 * Mathf.Deg2Rad), r1 * Mathf.Cos(thetha1 * Mathf.Deg2Rad)),
+                                        new Vector4(Mathf.Sin(thetha1 * Mathf.Deg2Rad), Mathf.Cos(thetha1 * Mathf.Deg2Rad) * Mathf.Cos(alpha1 * Mathf.Deg2Rad), -Mathf.Cos(thetha1 * Mathf.Deg2Rad) * Mathf.Sin(alpha1 * Mathf.Deg2Rad), r1 * Mathf.Sin(thetha1 * Mathf.Deg2Rad)),
+                                        new Vector4(0, Mathf.Sin(alpha1 * Mathf.Deg2Rad), Mathf.Cos(alpha1 * Mathf.Deg2Rad), d1),
+                                        new Vector4(0, 0, 0, 1));
+
+            // matrix T2
+            Matrix4x4 T2 = new Matrix4x4(new Vector4(Mathf.Cos(thetha2 * Mathf.Deg2Rad), -Mathf.Sin(thetha2 * Mathf.Deg2Rad) * Mathf.Cos(alpha2 * Mathf.Deg2Rad), Mathf.Sin(thetha2 * Mathf.Deg2Rad) * Mathf.Sin(alpha2 * Mathf.Deg2Rad), r2 * Mathf.Cos(thetha2 * Mathf.Deg2Rad)),
+                                        new Vector4(Mathf.Sin(thetha2 * Mathf.Deg2Rad), Mathf.Cos(thetha2 * Mathf.Deg2Rad) * Mathf.Cos(alpha2 * Mathf.Deg2Rad), -Mathf.Cos(thetha2 * Mathf.Deg2Rad) * Mathf.Sin(alpha2 * Mathf.Deg2Rad), r2 * Mathf.Sin(thetha2 * Mathf.Deg2Rad)),
+                                        new Vector4(0, Mathf.Sin(alpha2 * Mathf.Deg2Rad), Mathf.Cos(alpha2 * Mathf.Deg2Rad), d2),
+                                        new Vector4(0, 0, 0, 1));
+
+            // matrix T3
+            Matrix4x4 T3 = new Matrix4x4(new Vector4(Mathf.Cos(thetha3 * Mathf.Deg2Rad), -Mathf.Sin(thetha3 * Mathf.Deg2Rad) * Mathf.Cos(alpha3 * Mathf.Deg2Rad), Mathf.Sin(thetha3 * Mathf.Deg2Rad) * Mathf.Sin(alpha3 * Mathf.Deg2Rad), r3 * Mathf.Cos(thetha3 * Mathf.Deg2Rad)),
+                                        new Vector4(Mathf.Sin(thetha3 * Mathf.Deg2Rad), Mathf.Cos(thetha3 * Mathf.Deg2Rad) * Mathf.Cos(alpha3 * Mathf.Deg2Rad), -Mathf.Cos(thetha3 * Mathf.Deg2Rad) * Mathf.Sin(alpha3 * Mathf.Deg2Rad), r3 * Mathf.Sin(thetha3 * Mathf.Deg2Rad)),
+                                        new Vector4(0, Mathf.Sin(alpha3 * Mathf.Deg2Rad), Mathf.Cos(alpha3 * Mathf.Deg2Rad), d3),
+                                        new Vector4(0, 0, 0, 1));
+
+            // matrix T03
+            Matrix4x4 T03 = T1.transpose * T2.transpose * T3.transpose;
+
+            Matrix4x4 R03 = new Matrix4x4(T03.GetColumn(0), T03.GetColumn(1), T03.GetColumn(2), Vector4.zero);
+
+            // matrix R03T
+            Matrix4x4 R03T = R03.transpose;
+
+            // matrix R36
+            Matrix4x4 R36 = R03T * R.transpose;
+
+            // calculate thetha4
+            thetha4 = Mathf.Atan2(-R36[1, 2], -R36[0, 2]) * Mathf.Rad2Deg;
+
+            // calculate thetha5
+            thetha5 = Mathf.Acos(-R36[2, 2]) * Mathf.Rad2Deg - 180;
+
+            // calculate thetha6
+            thetha6 = Mathf.Atan2(-R36[2, 1], R36[2, 0]) * Mathf.Rad2Deg;
+
+            thetha3 = -thetha3 + 90;
+            if (checkRotate())
+            {
+                if (!realtime)
+                {
+                    addPointsList();
+                    //points.Add(new float[] { thetha1, thetha2, thetha3, thetha4, thetha5, thetha6 });
+                }
+                
+                print("End");
+                time.Stop();
+                print("ВРЕМЯ АЛГОРИТМА:" + time.ElapsedTicks);
+            }
+            else
+            {
+                UnityEngine.Debug.LogWarning("Выход за пределы расчетов!");
+
+            }
+            
+            // ModifyRobot(thetha1, thetha2, thetha3, thetha4, thetha5, thetha6);
+
+            /*q[0] = thetha1;
+            q[1] = thetha2;
+            q[2] = thetha3;
+            q[3] = thetha4;
+            q[4] = thetha5;
+            q[5] = thetha6;
+
+            qm[0] = (q[0] - q01) / Tm;
+            qm[1] = (q[1] - q02) / Tm;
+            qm[2] = (q[2] - q03) / Tm;
+            qm[3] = (q[3] - q04) / Tm;
+            qm[4] = (q[4] - q05) / Tm;
+            qm[5] = (q[5] - q06) / Tm;
+            qm[0] = (q[0] - q01);
+            qm[1] = (q[1] - q02);
+            qm[2] = (q[2] - q03);
+            qm[3] = (q[3] - q04);
+            qm[4] = (q[4] - q05);
+            qm[5] = (q[5] - q06);
+            
+            /*print(thetha1);
+            print(thetha2);
+            print(thetha3);
+            print(thetha4);
+            print(thetha5);
+            print(thetha6);
+            if (checkRotate())
+            {
+                StartCoroutine(LoopWithDelay(q01, q02, q03, q04, q05, q06));
+            }
+            else
+            {
+                UnityEngine.Debug.LogError("Выход за пределы расчетов!");
+                flagwork = false;
+            }*/
+        }
+        catch(Exception e)
+        {
+             UnityEngine.Debug.LogError("Ошибка в расчетах ИК!"+e);
+        }
+        
+         
+    }
+
+   /* public void BackToInitPos() // function to move robot tot initial position
+    {
+        thetha1 = 0;
+        thetha2 = 90;
+        thetha3 = 90;
+        thetha4 = 0;
+        thetha5 = -90;
+        thetha6 = 0;
+
+        q[0] = thetha1;
+        q[1] = thetha2;
+        q[2] = thetha3;
+        q[3] = thetha4;
+        q[4] = thetha5;
+        q[5] = thetha6;
+
+        qm[0] = (q[0] - q01) / Tm;
+        qm[1] = (q[1] - q02) / Tm;
+        qm[2] = (q[2] - q03) / Tm;
+        qm[3] = (q[3] - q04) / Tm;
+        qm[4] = (q[4] - q05) / Tm;
+        qm[5] = (q[5] - q06) / Tm;
+
+        RobotOperate = true;
+        StartCoroutine(LoopWithDelay(q01, q02, q03, q04, q05, q06));
+    }*/
+    bool checkRotate()
+    {
+        if (float.IsNaN(thetha1) || float.IsNaN(thetha2) || float.IsNaN(thetha3)|| float.IsNaN(thetha4) ||
+        float.IsNaN(thetha5) || float.IsNaN(thetha6))
+        {
+            return false;
+        }
+        else
+        {
+            return true;
+        }
+    }
+   
+    public void RobotStop() // stop the robot
+    {
+        RobotOperate = false;
+    }
+
+    /* System.Collections.IEnumerator LoopWithDelay(float q01, float q02, float q03, float q04, float q05, float q06)
+     {
+         for (int i = 1; i <= Tm; i++)
+         {
+             if (RobotOperate)
+             {
+                 q01 += qm[0]; q02 += qm[1]; q03 += qm[2]; q04 += qm[3]; q05 += qm[4]; q06 += qm[5];
+             }
+
+             thetha1 = q01;
+             thetha2 = q02;
+             thetha3 = q03;
+             thetha4 = q04;
+             thetha5 = q05;
+             thetha6 = q06;
+             ModifyRobot(q01, q02, q03, q04, q05, q06);
+
+             yield return new WaitForSeconds(0.05f); // how fast robot moves
+
+             if (!RobotOperate)
+             {
+                 flagwork = false;
+                 yield break;
+             }
+         }
+         flagwork = false;
+     }*/
+
+    private IEnumerator ModifyRobot(List<float?[]> points)
+    {
+        move = true;
+        RobotTest robotIns = configRobot.GetComponent<RobotTest>();
+        foreach (float?[] p in points)
+        {
+            if (RobotOperate)
+            {
+                robotIns.J1Angle = p[0] ?? robotIns.J1Angle;
+                robotIns.J2Angle = p[1] ?? robotIns.J2Angle;
+                robotIns.J3Angle = p[2] ?? robotIns.J3Angle;
+                robotIns.J4Angle = p[3] ?? robotIns.J4Angle;
+                robotIns.J5Angle = p[4] ?? robotIns.J5Angle;
+                robotIns.J6Angle = p[5] ?? robotIns.J6Angle;
+                yield return new WaitForSeconds(0);
+            }
+
+        }
+        move = false;
+    }
+    void ModifyRobotRealtime()
+    {
+        RobotTest robotIns = configRobot.GetComponent<RobotTest>();
+        robotIns.J1Angle = thetha1;
+        robotIns.J2Angle = thetha2;
+        robotIns.J3Angle = thetha3;
+        robotIns.J4Angle = thetha4;
+        robotIns.J5Angle = thetha5;
+        robotIns.J6Angle = thetha6;
+    }
+    /*while (true)
+    {
+        if (q01 != q[0])
+        {
+            robotIns.J1Angle = q[0];
+            q01 = q[0];
+        }
+        if (q02 != q[1])
+        {
+            robotIns.J2Angle = q[1];
+            q02 = q[1];
+        }
+        if (q03 != q[2])
+        {
+            robotIns.J3Angle = q[2];
+            q03 = q[2];
+        }
+        if (q04 != q[3])
+        {
+            robotIns.J4Angle = q[3];
+            q04 = q[3];
+        }
+        if (q05 != q[4])
+        {
+            robotIns.J5Angle = q[4];
+            q05 = q[4];
+        }
+        if(q06 != q[5])
+        {
+            robotIns.J6Angle = q[5];
+            q06 = q[5];
+        }
+         yield return new WaitForSeconds(0.01f);*/
+
+
+    /*RobotTest robotIns = configRobot.GetComponent<RobotTest>();
+
+    robotIns.J1Angle = a1;
+    robotIns.J2Angle = a2;
+    robotIns.J3Angle = a3;
+    robotIns.J4Angle = a4;
+    robotIns.J5Angle = a5;
+    robotIns.J6Angle = a6;
+
+    q01 = thetha1;
+    q02 = thetha2;
+    q03 = thetha3;
+    q04 = thetha4;
+    q05 = thetha5;
+    q06 = thetha6;*/
+
+    float?[] oldPoints = new float?[6];
+    void addPointsList()
+    {
+       // points.Add(new float?[]{thetha1, thetha2, thetha3, thetha4, thetha5, thetha6 });
+        float?[] angles = { null, null, null, null, null, null };
+        //oldPoints = new float?[] {thetha1 , thetha2, thetha3, thetha4, thetha5, thetha6};
+        if (thetha1 >= -175 & thetha1 <= 175)
+        {
+            angles[0] = thetha1;
+            oldPoints[0] = thetha1;
+        }
+        else
+        {
+            UnityEngine.Debug.LogWarning("ОГР А1");
+        }
+        if (thetha2 >= 20 && thetha2 <= 140)
+        {
+            angles[1] = thetha2;
+            oldPoints[1] = thetha2;
+        }
+        else
+        {
+            UnityEngine.Debug.LogWarning("ОГР А2");
+        }
+        if (thetha3 <= 140 && thetha3 >= 20)
+        {
+            angles[2] = thetha3;
+            oldPoints[2] = thetha3;
+        }
+        else
+        {
+            UnityEngine.Debug.LogWarning("ОГР А3");
+        }
+        if (thetha4 >= -180 && thetha4 <= 180)
+        {
+            angles[3] = thetha4;
+            oldPoints[3] = thetha4;
+        }
+        else
+        {
+            UnityEngine.Debug.LogWarning("ОГР А4");
+        }
+        if (thetha5 >= -105 && thetha5 <= 105)
+        {
+            angles[4] = thetha5;
+            oldPoints[4] = thetha5;
+        }
+        else
+        {
+            UnityEngine.Debug.LogWarning("ОГР А5");
+        }
+        if (thetha6 >= -360 && thetha6 <= 360)
+        {
+            angles[5] = thetha6;
+            oldPoints[5] = thetha6;
+        }
+        else
+        {
+            UnityEngine.Debug.LogWarning("ОГР А6");
+        }
+        points.Add(angles);
+    }
+
+}
---
+++ Assets/Robot files/Scripts/RobotTest.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class RobotTest : MonoBehaviour
+{
+    public GameObject J1;
+    public float J1Angle;
+    public GameObject J2;
+    public float J2Angle;
+    public GameObject J3;
+    public float J3Angle;
+    public GameObject J4;
+    public float J4Angle;
+    public GameObject J5;
+    public float J5Angle;
+    public GameObject J6;
+    public float J6Angle;
+
+    public GameObject EndEffector, BaseOrigin;
+
+    public InputField J1string, J2string, J3string, J4string, J5string, J6string, Xstring, Ystring, Zstring;
+    float y;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        y = J1Angle / 50;
+    }
+    
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+
+
+
+       // StartCoroutine(ky());
+        J1.transform.localRotation = Quaternion.AngleAxis(J1Angle + J2.transform.rotation.x, new Vector3(0, 1, 0));
+        J2.transform.localRotation = Quaternion.AngleAxis(-J2Angle + J2.transform.rotation.x, new Vector3(1, 0, 0));
+        J3.transform.localRotation = Quaternion.AngleAxis(J3Angle + J3.transform.rotation.x, new Vector3(1, 0, 0));
+        J4.transform.localRotation = Quaternion.AngleAxis(J4Angle + J4.transform.rotation.z, new Vector3(0, 0, 1));
+        J5.transform.localRotation = Quaternion.AngleAxis(-J5Angle + J5.transform.rotation.x, new Vector3(1, 0, 0));
+        J6.transform.localRotation = Quaternion.AngleAxis(J6Angle + J6.transform.rotation.z, new Vector3(0, 0, 1));
+        Physics.SyncTransforms();
+        /*J1.GetComponent<Rigidbody>().MoveRotation(Quaternion.AngleAxis(J1Angle, J1.GetComponentInParent<Transform>().TransformDirection(new Vector3(0,1,0))));
+        J2.GetComponent<Rigidbody>().MoveRotation(Quaternion.AngleAxis(-J2Angle,J2.GetComponentInParent<Transform>().TransformDirection( new Vector3(1, 0, 0))));
+        J3.GetComponent<Rigidbody>().MoveRotation(Quaternion.AngleAxis(J3Angle, J3.GetComponentInParent<Transform>().TransformDirection(new Vector3(1, 0, 0))));
+        J4.GetComponent<Rigidbody>().MoveRotation(Quaternion.AngleAxis(J4Angle, J4.GetComponentInParent<Transform>().TransformDirection(new Vector3(0, 0, 1))));
+        J5.GetComponent<Rigidbody>().MoveRotation(Quaternion.AngleAxis(-J5Angle, J5.GetComponentInParent<Transform>().TransformDirection(new Vector3(1, 0, 0))));
+        J6.GetComponent<Rigidbody>().MoveRotation(Quaternion.AngleAxis(J6Angle, J6.GetComponentInParent<Transform>().TransformDirection(new Vector3(0, 0, 1))));
+*/
+
+        /*J1string.text = Mathf.Round(-J1Angle).ToString();
+        J2string.text = Mathf.Round(-J2Angle).ToString();
+        J3string.text = Mathf.Round(J3Angle).ToString();
+        J4string.text = Mathf.Round(J4Angle).ToString();
+        J5string.text = Mathf.Round(J5Angle).ToString();
+        J6string.text = Mathf.Round(-J6Angle).ToString();
+
+        // EE position
+        Xstring.text = ((EndEffector.transform.position.z - BaseOrigin.transform.position.z) * 1000).ToString();
+        Ystring.text = ((EndEffector.transform.position.x - BaseOrigin.transform.position.x) * 1000).ToString();
+        Zstring.text = ((EndEffector.transform.position.y - BaseOrigin.transform.position.y) * 1000).ToString();*/
+    }
+    IEnumerator ky()
+    {
+        float ygol = -175;
+        for (int i = 0; i < 50; i++)
+        {
+            ygol += y;
+            J1.transform.localRotation = Quaternion.AngleAxis( y, new Vector3(0, 1, 0));
+            yield return new WaitForSeconds(0.1f);
+        }
+    }
+}
