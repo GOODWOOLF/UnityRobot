024b7a17919575bb784ef9c6b949643d142a5735
Denis <denis_simonov1337@mail.ru>
2025-10-22 01:48:12 +0500

    add magnit

--- Assets/Robot files/Scripts/Magnit.cs
+++ Assets/Robot files/Scripts/Magnit.cs
@@ -8,9 +8,11 @@ public class Magnit : MonoBehaviour
     public float magnitR = 0.1f;
     public string[] magneticTags = { "Magnetic" };
     public bool magnitOn = false;
+    private MeshRenderer renderer;
+    private bool isObjectMagnet;
     void Start()
     {
-        
+        renderer = GetComponent<MeshRenderer>();
     }
 
     // Update is called once per frame
@@ -18,15 +20,40 @@ public class Magnit : MonoBehaviour
     {
         if (magnitOn)
         {
+            if (isObjectMagnet) return;
             Collider[] colliders = Physics.OverlapSphere(transform.position, magnitR);
             foreach (Collider col in colliders)
             {
                 if (magneticTags.Contains(col.gameObject.tag))
                 {
-                    Vector3 direction = (transform.position - col.gameObject.transform.position).normalized;
-                    col.GetComponent<Rigidbody>().AddForce(direction * magnitF);
+                    var rb = col.GetComponent<Rigidbody>();
+                    rb.linearVelocity = (transform.position - (rb.transform.position + rb.centerOfMass)) * magnitF* Time.fixedDeltaTime;
+                    //Vector3 direction = (transform.position - col.gameObject.transform.position).normalized;
+                    //.AddForce(direction * magnitF);
                 }
             }
         }
+    }
+    private void OnCollisionEnter(Collision collision)
+    {
+        isObjectMagnet = true;
+    }
+    private void OnCollisionStay(Collision collision)
+    {
+        if (isObjectMagnet && magnitOn)
+        {
+            var rb = collision.rigidbody;
+            
+            rb.transform.parent = transform;
+            rb.useGravity = false;
+            //print(transform.position.y + " | "  + size.y + " = " + (transform.position.y - size.y));
+            //rb.position = Vector3.MoveTowards(transform.position.x, transform.position.y - size.y, transform.position.z);
+        }
+    }
+    private void OnCollisionExit(Collision collision)
+    {
+        var rb = collision.rigidbody;
+        isObjectMagnet = false;
+        rb.useGravity = true;
     }
 }
