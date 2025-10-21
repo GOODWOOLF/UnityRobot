8deb73b61383201c1fb342964e763887d7f2abc5
GOODWOOLF <electromolot@mail.ru>
2025-10-22 03:27:14 +0500

    рабочий магнит

--- Assets/Robot files/Scripts/Magnit.cs
+++ Assets/Robot files/Scripts/Magnit.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
 
@@ -8,11 +9,10 @@ public class Magnit : MonoBehaviour
     public float magnitR = 0.1f;
     public string[] magneticTags = { "Magnetic" };
     public bool magnitOn = false;
-    private MeshRenderer renderer;
-    private bool isObjectMagnet;
+    private bool isObjectMagnet = false;
+    private List<GameObject> magnited = new();
     void Start()
     {
-        renderer = GetComponent<MeshRenderer>();
     }
 
     // Update is called once per frame
@@ -27,33 +27,54 @@ public class Magnit : MonoBehaviour
                 if (magneticTags.Contains(col.gameObject.tag))
                 {
                     var rb = col.GetComponent<Rigidbody>();
-                    rb.linearVelocity = (transform.position - (rb.transform.position + rb.centerOfMass)) * magnitF* Time.fixedDeltaTime;
-                    //Vector3 direction = (transform.position - col.gameObject.transform.position).normalized;
-                    //.AddForce(direction * magnitF);
+                    
+                    // Получаем ближайшую точку на коллайдере магнита
+                    Vector3 closestPoint = GetComponent<Collider>().ClosestPoint(col.transform.position);
+                    
+                    // Направление к ближайшей точке на магните
+                    Vector3 direction = (closestPoint - rb.transform.position).normalized;
+                    
+                    rb.AddForce(direction * magnitF, ForceMode.Force);
                 }
             }
+        }
+        else if (isObjectMagnet)
+        {
+            foreach (GameObject obj in magnited)
+            {
+                obj.transform.parent.SetParent(null);
+                obj.GetComponent<Rigidbody>().isKinematic = false;
+            }
+            magnited.Clear();
+            isObjectMagnet = false;
         }
     }
     private void OnCollisionEnter(Collision collision)
     {
-        isObjectMagnet = true;
-    }
-    private void OnCollisionStay(Collision collision)
-    {
-        if (isObjectMagnet && magnitOn)
+        if (magnitOn)
         {
             var rb = collision.rigidbody;
-            
-            rb.transform.parent = transform;
-            rb.useGravity = false;
-            //print(transform.position.y + " | "  + size.y + " = " + (transform.position.y - size.y));
-            //rb.position = Vector3.MoveTowards(transform.position.x, transform.position.y - size.y, transform.position.z);
+            rb.transform.parent.SetParent(transform);
+            magnited.Add(collision.gameObject);
+            rb.isKinematic = true;
+            isObjectMagnet = true;
         }
+
     }
-    private void OnCollisionExit(Collision collision)
+
+     public Color magnetColor = new Color(0, 1, 1, 0.3f);
+    
+    void OnDrawGizmos()
     {
-        var rb = collision.rigidbody;
-        isObjectMagnet = false;
-        rb.useGravity = true;
+        if (magnitOn)
+        {
+            // Рисуем сферу радиуса магнита
+            Gizmos.color = magnetColor;
+            Gizmos.DrawWireSphere(transform.position, magnitR);
+            
+            // Дополнительно заливаем прозрачной сферой
+            Gizmos.color = new Color(magnetColor.r, magnetColor.g, magnetColor.b, 0.1f);
+            Gizmos.DrawSphere(transform.position, magnitR);
+        }
     }
 }
